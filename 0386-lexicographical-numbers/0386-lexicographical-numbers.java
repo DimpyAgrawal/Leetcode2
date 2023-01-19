bd3033d1785fec82de8dc74085b98bc85ec3860a
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-01-19 11:38:05 +0530

    Time: 4 ms (82.37%), Space: 47.1 MB (62.20%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        ArrayList<Integer> t= new ArrayList<Integer>();
+        lexicoCounting(0,n,t);   
+        return t;
+    }
+    	    	
+	
+	public static void lexicoCounting(int curr,int n,ArrayList<Integer> t) {
+		
+        if(curr>n)
+			return;
+		if(curr!=0)
+            t.add(curr);
+		int i=0;
+		if(curr==0)
+			i= 1;
+		
+		for(;i<=9;i++) {
+			lexicoCounting(10*curr+i,n,t);
+		}
+		
+}
+}
