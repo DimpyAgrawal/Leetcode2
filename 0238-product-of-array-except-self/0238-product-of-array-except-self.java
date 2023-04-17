c42a5b92ccb575578bdec8f98896960c0400264d
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-04-17 22:45:18 +0530

    Time: 2 ms (49.82%), Space: 50.3 MB (95.44%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+       // int[] ans =product_of_array_except_itself(nums);
+       return product_of_array_except_itself(nums);
+    
+    }
+     public static int[] product_of_array_except_itself(int[] arr){
+        int[] left=new int[arr.length];
+        left[0]=1;
+        for(int i=1;i<arr.length;i++){
+            left[i]=left[i-1]*arr[i-1];
+        }
+        int[] right=new int[arr.length];
+        right[arr.length-1]=1;
+        for(int i=arr.length-2;i>=0;i--){
+            right[i]=right[i+1]*arr[i+1];
+        }
+        for(int i=0;i<arr.length;i++){
+            left[i]*=right[i];
+        }
+      return left;  
+     }
+}
