67089d5f7420c273fba7aabdb4ccff0911e40a96
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-05-19 18:08:43 +0530

    Time: 4 ms (90.22%), Space: 46 MB (84.89%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,31 +1,43 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-//         int count=0;
-//         int sum=0;
-//         for(int i=0;i<nums.length;i++){
-//             for(int j=i;j<nums.length;j++){
-//                 sum=0;
-//                 for(int k1=i;k1<=j;k1++){
-//                      sum+=nums[k1];
-//                 }
-//                   if(sum%k==0)
-//                 count++;
-//             }
-           
-          
-//         }
-//         return count;
+        // int count=0;
+        // int sum=0;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i;j<nums.length;j++){
+        //         sum=0;
+        //         for(int k1=i;k1<=j;k1++){
+        //              sum+=nums[k1];
+        //         }
+        //           if(sum%k==0)
+        //         count++;
+        //     }
+        // }
+        // return count;
         
         
-        int n=nums.length;
-        int sum=0;
-        int ans=0;
-        int[] arr=new int[k];
-        arr[0]=1;
-        for(int a:nums){
-            sum = (sum+a % k + k) % k;
-            ans+=arr[sum]++;
+       //  int n=nums.length;
+       //  int sum=0;
+       //  int ans=0;
+       // int[] arr=new int[k];
+       //  arr[0]=1;
+       //  for(int a:nums){
+       //      sum = (sum + a % k + k) % k;
+       //      ans+= arr[sum]++;
+       //  }
+       //  return ans;
+        
+        
+         int[] count = new int[k];
+    int prefixSum = 0, result = 0;
+    count[0] = 1;
+    for (int num : nums) {
+        prefixSum = (prefixSum + num) % k;
+        if (prefixSum < 0) {
+            prefixSum += k;
         }
-        return ans;
+        result += count[prefixSum];
+        count[prefixSum]++;
+    }
+    return result;
     }
 }
