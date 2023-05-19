390977ebe7f8b63ac1f01904688cc456a50ddb62
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-05-19 19:37:29 +0530

    Time: 6 ms (53.38%), Space: 49.5 MB (14.77%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,44 +1,24 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-//         int maxsum= Integer.MIN_VALUE;
-//         int sum=nums[0];
-//         int[] arr=new int[2*nums.length];
-//         int k=0;
-//         for(int i=0;i<nums.length;i++){
-//             arr[i]=nums[i];
-//         }
+
+        int max= Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int totalsum=0;
+        int sum=0;
+        int sum2=0;
+        for(int i=0;i<nums.length;i++){
+            totalsum+=nums[i];
+           // sum+=nums[i];
+            sum=Math.max(sum+nums[i],nums[i]);
+            max=Math.max(max,sum);
+            sum2=Math.min(sum2+nums[i],nums[i]);
+
+            min=Math.min(min,sum2);
             
-//         for(int i=nums.length;i<arr.length;i++){
-//             arr[i]=nums[k];
-           
-//             k++;
-//         }
-       
-        
-//         for(int i=0;i<arr.length;i++){
-//          sum+=arr[i];
-            
-//         maxsum=Math.max(maxsum,sum);
-//             if(sum<0){
-//                 sum=0;
-//             }
-                
-          
-        
-            
-            
-//         }
-           
-        
-//        return maxsum;
-        int total = 0, maxSum = nums[0], curMax = 0, minSum = nums[0], curMin = 0;
-        for (int a : nums) {
-            curMax = Math.max(curMax + a, a);
-            maxSum = Math.max(maxSum, curMax);
-            curMin = Math.min(curMin + a, a);
-            minSum = Math.min(minSum, curMin);
-            total += a;
         }
-        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+       if(max<0) return max;
+        
+        return Math.max(max,totalsum-min);
     }
 }
+
