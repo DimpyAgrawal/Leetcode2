bd8a88bc5f586ea4e5f85cb601906e5a1a1f7087
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-05-31 17:34:41 +0530

    Time: 2 ms (80.63%), Space: 43.9 MB (16.22%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> ll= new ArrayList<>();
-		List<List<Integer>> ans= new ArrayList<>();
-		
-		combination_sum( candidates,target,ll,ans,0 );
+     
+        List<Integer> ll=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        
+        
+        combination(candidates, ll, ans,0,target);
         return ans;
+	}
+    
+    public static void combination(int[] arr, List<Integer> ll,  List<List<Integer>> ans,int sum, int target){
+        if(target==0){
+            ans.add(new ArrayList<>(ll));
+            return;
+        }
+        for(int i=sum;i<arr.length;i++){
+            if(target>=arr[i]){
+                ll.add(arr[i]);
+            combination(arr,ll,ans,i,target-arr[i]);
+            ll.remove(ll.size()-1);
+            }
+        }
     }
-    public static void combination_sum(int[] arr,int target,List<Integer> ll,List<List<Integer>> ans,int sum ) {
-			if(target==0) {
-				//System.out.println(ans);
-				ans.add(new ArrayList<>(ll));
-				return;
-			}
-			for(int i=sum;i<arr.length;i++) {
-				if(target>=arr[i]){
-					ll.add(arr[i]);
-				combination_sum( arr,target-arr[i],ll,ans,i );
-				ll.remove(ll.size()-1);
-                }
-			}
-				
-		}
 }
