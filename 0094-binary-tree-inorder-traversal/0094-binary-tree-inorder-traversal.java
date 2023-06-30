f0703acb24ee706ce136ff664e2c462673dcbf99
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-06-30 17:54:12 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (87.11%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,25 +13,71 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         ArrayList<Integer> list =  new ArrayList<>();
+//         return inorder(root,list);     
+//     }
+//     public static ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> list){
+//         if(root==null){
+//             return list;          //recursive approach
+//         }
+        
+//         inorder(root.left,list);
+//         list.add(root.val);
+//         inorder(root.right,list);
+//        return list; 
+        
+// }       
+// }       
+        
+//         ArrayList<Integer> list =new ArrayList<>();
+//         Stack<TreeNode> st=new Stack<>();
+//         TreeNode node= root;
+        
+//         while(true){
+            
+//             if(node!=null){
+//                 st.push(node);
+//                 node=node.left;
+//             }
+//             else{
+//                 if(st.isEmpty()){
+//                 break;
+                
+//             } 
+//             }
+//            node=st.pop();
+//             list.add(node.val);
+//             node=node.right;
+            
+//         }
+//         return list;
+//     }
+    
+// }
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        ArrayList<Integer> list =  new ArrayList<>();
-        
-        return inorder(root,list);
-        
-        
-    }
-    public static ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> list){
-        if(root==null){
-            return list;
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+
+        while (node != null || !stack.isEmpty()) {
+            // Traverse to the leftmost node of the current subtree
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+
+            // Process the current node
+            node = stack.pop();
+            list.add(node.val);
+
+            // Move to the right subtree
+            node = node.right;
         }
-        
-        inorder(root.left,list);
-        list.add(root.val);
-        inorder(root.right,list);
-       return list; 
-        
+
+        return list;
     }
-    
-    
 }
