d5fad7588f23373f24890d9acdd96a84b44bf661
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-07-17 18:20:08 +0530

    Time: 9 ms (73.55%), Space: 44.1 MB (53.99%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,42 +1,26 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
       
-//         int pairs = 0, n = nums.length;
+        int pairs = 0, n = nums.length;
         
-//         HashSet<Integer> set = new HashSet<>();
-//         HashSet<Integer> repeatedElements = new HashSet<>();
-        
-//         for(int ele : nums){
-//             if(set.contains(ele)) repeatedElements.add(ele);
-//             set.add(ele);
-//         }
-        
-//         // SPECIAL CASE [K == 0]
-//         if(k == 0) return repeatedElements.size();
-        
-//         // Traversing the SET not the array.
-//         for(int elementInSet : set)
-//             if(set.contains(elementInSet + k)) pairs++;
-       
-//         return pairs;
-//     }
-// }
-        
-        
-        Set<Integer> set= new HashSet<>();
-        Set<Integer> repeatedele= new HashSet<>();
-        int c=0;
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> repeatedElements = new HashSet<>();
         
         for(int ele : nums){
-            if(set.contains(ele)) repeatedele.add(ele);
+            if(set.contains(ele)) repeatedElements.add(ele);
             set.add(ele);
         }
         
-        if(k==0) return repeatedele.size();
+        // SPECIAL CASE [K == 0]
+        if(k == 0) return repeatedElements.size();
         
-        for(int ele:set)
-            if(set.contains(ele+k)) c++;
-        
-        return c;
+        // Traversing the SET not the array.
+        for(int elementInSet : set)
+            if(set.contains(elementInSet + k)) pairs++;
+       
+        return pairs;
     }
 }
+        
+        
+       
