0772013ec641139c8ecd9a9ab80f75050f630501
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-07-19 22:37:48 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (11.39%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int c=image[0].length;
-        for(int[] row : image)
+        for(int[] row : image){
             for(int i=0;i<(c+1)/2;i++){
                 int temp=row[i]^1;
                 row[i] =row[c-i-1]^1;
                 row[c-i-1]=temp;
             }
+        }
         
         return image;
     }
