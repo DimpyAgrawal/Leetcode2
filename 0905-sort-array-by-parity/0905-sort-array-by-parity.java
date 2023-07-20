f90eca22cd3ef4092603da0d9ea7d28fd435510f
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-07-20 07:57:58 +0530

    Time: 1 ms (99.05%), Space: 44.1 MB (72.56%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,18 +1,30 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int n= nums.length;
-        int t=0;
-        int[] ans= new int[n];
+//         int n= nums.length;
+//         int t=0;
+//         int[] ans= new int[n];
         
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0)
-                ans[t++]=nums[i];  
-        }
+//         for(int i=0;i<n;i++){
+//             if(nums[i]%2==0)
+//                 ans[t++]=nums[i];  
+//         }
         
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==1)
-                ans[t++]=nums[i];
+//         for(int i=0;i<n;i++){
+//             if(nums[i]%2==1)
+//                 ans[t++]=nums[i];
+//         }
+//         return ans;
+        
+        int i=0,j=nums.length-1;
+        while(i<j){
+            if(nums[i]%2>nums[j]%2){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+            if(nums[i]%2==0) i++;
+            if(nums[j]%2==1) j--;
         }
-        return ans;
+        return nums;
     }
 }
