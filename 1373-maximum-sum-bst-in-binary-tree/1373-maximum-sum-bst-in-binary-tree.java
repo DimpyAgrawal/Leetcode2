39013f76fcecc911ab86c096035d3df6cc5ac860
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-07-29 19:57:33 +0530

    Time: 7 ms (98.88%), Space: 59.1 MB (53.99%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -13,36 +13,35 @@
  *     }
  * }
  */
- class NodeValue{
+class NodeValue{
     public int maxValue,minValue,maxsum;
-    NodeValue(int max, int min,int maxsum){
-        this.maxValue = max;
-        this.minValue = min;
-        this.maxsum = maxsum;
+    NodeValue(int max,int min,int maxsum){
+         this.maxValue = max;
+         this.minValue = min;
+         this.maxsum = maxsum ;
+        
     }
 }
-
-class Solution {
-    int max = 0;
-    public NodeValue maxSumBSTHelper(TreeNode root){
-        //An empty tree or bst of size 0;
-        if(root == null){
-            return new NodeValue(Integer.MIN_VALUE,Integer.MAX_VALUE,0);
-        }
-        NodeValue left = maxSumBSTHelper(root.left);
-        NodeValue right = maxSumBSTHelper(root.right);
-        
-        //Check if it's a valid bst
-        if(left.maxValue < root.val && root.val < right.minValue){
-            //It is a BST , calculate maxsum tillnow.
-             max = Math.max(max,root.val + left.maxsum + right.maxsum);
-            return new NodeValue(Math.max(root.val,right.maxValue), Math.min(root.val,left.minValue),root.val+left.maxsum + right.maxsum);
-           
-        }
-        return new NodeValue(Integer.MAX_VALUE,Integer.MIN_VALUE,Math.max(left.maxsum,right.maxsum));
-    }
+class Solution{
+    int max=0;
     public int maxSumBST(TreeNode root) {
-        maxSumBSTHelper(root);
+        maxSumHelper(root);
         return max;
     }
+
+public NodeValue maxSumHelper(TreeNode root){
+   if(root == null){
+            return new NodeValue(Integer.MIN_VALUE,Integer.MAX_VALUE,0);
+        }
+    
+    NodeValue left = maxSumHelper(root.left);
+    NodeValue right = maxSumHelper(root.right);
+    
+    if(left.maxValue<root.val && root.val<right.minValue){
+        max= Math.max(max,left.maxsum+right.maxsum+root.val);
+        return new NodeValue(Math.max(root.val,right.maxValue),Math.min(root.val,left.minValue),left.maxsum+right.maxsum+root.val);
+    }
+    return new NodeValue(Integer.MAX_VALUE,Integer.MIN_VALUE,Math.max(left.maxsum,right.maxsum));
 }
+}
+
