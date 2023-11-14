1be25915224afc35ebd2bbca354c7f2208ae85de
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2023-11-14 12:32:58 +0530

    Time: 15 ms (57.66%), Space: 44.9 MB (44.04%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div503-next-greater-element-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div503-next-greater-element-ii.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div503-next-greater-element-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div503-next-greater-element-ii.java
@@ -1,20 +1,15 @@
 class Solution {
     public int[] nextGreaterElements(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
-        int[] nxtG = new int[arr.length];
-        for (int i = 2 * arr.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= arr[i % arr.length]){
-                stack.pop();
+        Stack<Integer> st = new Stack<>();
+        int[] ans  = new int[arr.length];
+        int n = arr.length;
+        Arrays.fill(ans,-1);
+        for(int i=0;i<n*2;i++){
+            while(!st.isEmpty() && arr[st.peek()]<arr[i%n]){
+                ans[st.pop()] = arr[i%n];
             }
-            if(i < arr.length){
-                if(!stack.isEmpty()){
-                    nxtG[i] = stack.peek();
-                }else {
-                    nxtG[i] = -1;
-                }
-            }
-            stack.push(arr[i % arr.length]);
+            st.push(i%n);
         }
-        return nxtG;
+        return ans;
     }
 }
