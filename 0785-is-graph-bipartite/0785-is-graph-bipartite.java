7c0b7e84357c09367aae7d8de06e07b5bf2fe586
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2024-01-10 22:13:08 +0530

    Time: 8 ms (12.13%), Space: 46 MB (7.29%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,30 +1,32 @@
 class Solution {
     public boolean isBipartite(int[][] arr) {
-        int n=arr.length;
-        int[] color = new int[n];
-        Arrays.fill(color,-1);
-        for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                if(check(i,color,arr)==false) return false;
-            }
-        }
-       return true;
-    }
-    static boolean check(int start,int[] color,int[][] arr){
-        Queue<Integer> q= new LinkedList<>();
-        q.add(start);
-        color[start]=0;
-        
-        while(!q.isEmpty()){
-            int node =  q.remove();
-            for(int it: arr[node]){
-                if(color[it]==color[node]) return false;
-                else if(color[it]==-1){
-                    color[it]=1-color[node];
-                    q.add(it);
+        HashMap<Integer,Integer> visited = new HashMap<>();
+        Queue<BipertitePair> q = new LinkedList<>();
+        for(int i=0;i<arr.length;i++){
+            if(visited.containsKey(i)) continue;
+            q.add(new BipertitePair(i,0));
+            while(!q.isEmpty()){
+                BipertitePair rp = q.poll();
+                if(visited.containsKey(rp.vtx)){
+                    if(visited.get(rp.vtx)!=rp.des) return false;
+                    continue;
+                }
+                visited.put(rp.vtx,rp.des);
+                for(int nbrs: arr[rp.vtx]){
+                    if(!visited.containsKey(nbrs))
+                        q.add(new BipertitePair(nbrs,rp.des+1));
                 }
             }
         }
         return true;
+    }
+
+    class BipertitePair{
+        int vtx;
+        int des;
+        public BipertitePair(int vtx,int des){
+            this.vtx = vtx;
+            this.des = des;
+        }
     }
 }
