9d835b1abdb4f47d997c3c7ee579ebac6cf88bd1
Dimpy Agrawal <93111017+DimpyAgrawal@users.noreply.github.com>
2024-02-20 11:13:59 +0530

    Time: 195 ms (24.31%), Space: 55.5 MB (85.40%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3043-find-the-length-of-the-longest-common-prefix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3043-find-the-length-of-the-longest-common-prefix.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3043-find-the-length-of-the-longest-common-prefix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,18 +1,21 @@
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        HashSet<String> set = new HashSet<>();
-        for(int ele : arr1){
-            String s = ele+"";
-            for(int i=0;i<s.length();i++)
-                set.add(s.substring(0,i+1));
+      HashSet<String> set = new HashSet<>();
+        for(int i=0;i<arr1.length;i++){
+          
+            String s = Integer.toString(arr1[i]);
+            for(int j=0;j<s.length();j++)
+                set.add(s.substring(0,j+1));
         }
-        int max = 0;
-        for(int ele : arr2){
-            String s = ele+"";
-            for(int i=0;i<s.length();i++){
-                String str = s.substring(0,i+1);
-                if(set.contains(str))
-                    max = Math.max(max,str.length());
+        int max=0;
+         for(int i=0;i<arr2.length;i++){
+            String s = Integer.toString(arr2[i]);
+            for(int j=0;j<s.length();j++){
+                if(set.contains(s.substring(0,j+1))){
+                    String a = s.substring(0,j+1);
+                    max = Math.max(max,a.length());
+                }
+                
             }
         }
         return max;
